19421e5db608eb4324dc227f79eac5f17ccc45de
prabha haran <hprabha72@gmail.com>
2025-03-02 08:29:04 +0530

    new changes

--- Backend/src/controller/auth.control.js
+++ Backend/src/controller/auth.control.js
@@ -2,11 +2,8 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utill.js";
 import cloudinaryConfig from "../lib/cloudinary.js";
-import {v2 as cloudinary} from 'cloudinary';
-
-
-
-
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 /* 
 Post 
@@ -14,7 +11,7 @@ for sign in
  */
 
 export const signUp = async (req, res) => {
-  const { fullName, email, password ,profilePic} = req.body;
+  const { fullName, email, password, profilePic } = req.body;
   try {
     if (password.length < 6) {
       return res
@@ -32,7 +29,7 @@ export const signUp = async (req, res) => {
       fullName,
       email,
       password: hashedPassword,
-      profilePic
+      profilePic,
     });
 
     if (newUser) {
@@ -41,68 +38,60 @@ export const signUp = async (req, res) => {
 
       const savedUser = await newUser.save();
 
-      res.status(201).json(savedUser)
+      res.status(201).json(savedUser);
+    } else {
+      res.status(400).json({ message: "Failed to create user" });
     }
-    else{
-        res.status(400).json({ message: "Failed to create user" });
-    }
-
-
   } catch (error) {
-    console.log("Error in signup controller :", error.message)
+    console.log("Error in signup controller :", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
 
 /* 
 POST
 for user LOGIN
  */
 export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  try{
-  const { email, password } = req.body;
-
-  const user =  await User.findOne({ email });
-  if (!user) {
-    return res.status(400).json({ message: "invalid credenttials" });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "invalid credenttials" });
     }
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
       return res.status(400).json({ message: "invalid credetentials" });
-      }
-      // generate jwt token here
-      generateToken(user._id, res);
-      res.status(200).json(
-        {
-          message: "logged in successfully",
-          _id : user._id,
-          fullName : user.fullName,
-          email : user.email,
-          profilePic : user.profilePic
-        }
-      );
-      } catch (error) {
-        console.log("Error in login controller :", error.message)
-        res.status(500).json({ message: "interneal server Error" });
-        };
-      };
+    }
+    // generate jwt token here
+    generateToken(user._id, res);
+    res.status(200).json({
+      message: "logged in successfully",
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller :", error.message);
+    res.status(500).json({ message: "interneal server Error" });
+  }
+};
 
-      /* 
+/* 
       POST
       for user LOGOUT
        */
 
 export const logout = (req, res) => {
-try {
-  res.cookie("jwt", '' ,{maxAge:0});
-  res.status(200).json({message:"User Logged Out Successfully"})
-} catch (error) {
-  console.log('Error in logout controller:', error.message);
-  res.status(500).json({message:"Internal server error"})
-}
-
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "User Logged Out Successfully" });
+  } catch (error) {
+    console.log("Error in logout controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 /* 
@@ -112,13 +101,19 @@ for update user profile
 
 export const updateProfilePic = async (req, res) => {
   try {
-    console.log(req.file);
-    console.log(req.body)
     const user = await User.findById(req.user._id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
+    if (user) {
+      fs.unlink(req.user.profilePic, (err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log("previous pic  is deleted");
+        }
+      });
+    }
     const userId = req.user._id;
     const updatedUser = await User.findByIdAndUpdate(
       userId,
@@ -130,51 +125,75 @@ export const updateProfilePic = async (req, res) => {
       message: "User Profile Updated Successfully",
       updatedUser,
     });
+  } catch (error) {
+    console.log("Error in update profile controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-        } catch (error) {
-          console.log('Error in update profile controller:', error.message);
-          res.status(500).json({ message: "Internal server error" });
-          }
-      };
-
-      /* 
+/* 
       Put
       FOR UPDATING USER DETAILS 
        */
-      export const updateProfileDetail = async (req, res) => {
-        try {
-          const user = await User.findById(req.user._id);
-          if (!user) {
-            return res.status(404).json({ message: "User not found" });
-            }
-            const {fullName,bio } = req.body;
-            if (!fullName) {
-              return res.status(400).json({ message: "FullName is required" });
-            };
-            const userId = req.user._id;
-          
-            const updatedUser = await User.findByIdAndUpdate(userId, {
-              fullName:fullName,
-              bio:bio,
-              }, { new: true });
-              res.status(200).json({ message: "User Profile Details Updated Successfully" , updatedUser});
-              } catch (error) {
-                console.log('Error in update profile details controller:', error.message);
-                res.status(500).json({ message: "Internal server error" });
-                }
-            };
-      /*  
+export const updateProfileDetail = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const { fullName, bio } = req.body;
+    if (!fullName) {
+      return res.status(400).json({ message: "FullName is required" });
+    }
+    const userId = req.user._id;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        fullName: fullName,
+        bio: bio,
+      },
+      { new: true }
+    );
+    res.status(200).json({
+      message: "User Profile Details Updated Successfully",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log("Error in update profile details controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+/*  
       GET 
       FOR  chek authentication
        */
-      export const checkAuth = (req, res) => {
-        try {
-      
-            res.status(200).json({ message: "User is authenticated" , user: req.user
-            });
-          
-            } catch (error) {
-              console.log('Error in check auth controller:', error.message);
-              res.status(500).json({ message: "Internal server error" });
-              }
-              };
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json({ message: "User is authenticated", user: req.user });
+  } catch (error) {
+    console.log("Error in check auth controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+/* 
+              DELETE
+              TO REMOVE USER ACCOUNT AND DATAS
+               */
+
+export const removeUserAccount = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const findUser = await User.find(userId);
+    if (!findUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const user = await User.findByIdAndDelete(userId);
+    res.status(200).json({ message: "User account deleted successfully" });
+    // res.status(200).json({ message: "User account deleted successfully", user });
+  } catch (error) {
+    console.log("Error in remove user account controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- Backend/src/controller/message.control.js
+++ Backend/src/controller/message.control.js
@@ -1,26 +1,24 @@
 import cloudinaryConfig from "../lib/cloudinary.js";
-import  {ReciverScoketId}  from "../lib/socket.js";
+import { ReciverScoketId } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 import { io } from "../lib/socket.js";
 
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
 
-
-export const getUsersForSidebar = async (req , res ) =>{
-    try {
-        const loggedInUserId = req.user._id
-
-        const filteredUsers = await User.find( { _id: { $ne: loggedInUserId } } ).select("-password")
-        if(!filteredUsers){
-            return res.status(404).json({ message: "No users found" })
-        }
-        res.status(200).json(filteredUsers);
-        } catch (error) {
-            console.log("Error in fetching user for sidebar:", error.message)
-            res.status(500).json({ message: "Failed to fetch users" });
-            }
-
-
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    if (!filteredUsers) {
+      return res.status(404).json({ message: "No users found" });
+    }
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in fetching user for sidebar:", error.message);
+    res.status(500).json({ message: "Failed to fetch users" });
+  }
 };
 
 /* 
@@ -28,70 +26,111 @@ GET
 to fetch the messages between two users 
  */
 
-export const getMessages = async (req , res ) =>{
-    try {
-        const loggedInUserId = req.user._id
-      
-        const { _id:userToChatId} = req.params
-        
-        const messages = await Message.find({
-            $or: [
-              { senderId: loggedInUserId, reciverId: userToChatId },
-              { senderId: userToChatId, reciverId: loggedInUserId }
-            ]
-          }).sort({ createdAt: 1 });
-        
-       res.status(200).json(messages)
-    }
-    catch{
-        console.log("Error in fetching messages:", error.message)
-        res.status(500).json({ message: "Failed to fetch messages" });
-        
-    }
-    }
+export const getMessages = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
 
-    /* 
+    const { _id: userToChatId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: loggedInUserId, reciverId: userToChatId },
+        { senderId: userToChatId, reciverId: loggedInUserId },
+      ],
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
+  } catch {
+    console.log("Error in fetching messages:", error.message);
+    res.status(500).json({ message: "Failed to fetch messages" });
+  }
+};
+
+/* 
     POST
     FOR SENDING MESSAGES AND STORING
      */
 
-    export const sendMessages = async (req , res ) =>{
-        try {
-            const senderId= req.user._id;
-           
-            const { _id:reciverId} = req.params;
-            const {text , image} = req.body;
+export const sendMessages = async (req, res) => {
+  try {
+    const senderId = req.user._id;
 
-           let imageUrl;
-           if(image){
-            const uploadResponse = await cloudinary.uploader.upload(image);
-            imageUrl = uploadResponse.secure_url;
-        } 
-        const message = new Message({
-            senderId: senderId ,
-            reciverId: reciverId ,
-            text ,
-            image: imageUrl ,
-            })
-          const savedMessage = await message.save()
+    const { _id: reciverId } = req.params;
+    const { text, image } = req.body;
 
-//toto real time chat with socket.io
-    const reciverScoketId= ReciverScoketId(reciverId);
-    if(reciverScoketId){
-        io.to(reciverScoketId).emit('newMessage', savedMessage);
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const message = new Message({
+      senderId: senderId,
+      reciverId: reciverId,
+      text,
+      image: imageUrl,
+    });
+    const savedMessage = await message.save();
+
+    const reciverScoketId = ReciverScoketId(reciverId);
+    if (reciverScoketId) {
+      io.to(reciverScoketId).emit("newMessage", savedMessage);
     }
 
-          res.status(201).json({
-            message: "Message sent successfully",
-            savedMessage
-          });
+    res.status(201).json({
+      message: "Message sent successfully",
+      savedMessage,
+    });
+  } catch (error) {
+    console.log("Error in sending message controller:", error.message);
+    res.status(500).json({ message: "Failed to send message" });
+  }
+};
 
-
+/* 
+    PUT 
+    FOR EDITING THE MESSAGE
+    */
+export const editMessage = async (req, res) => {
+  try {
+    const { _id: messageId } = req.params;
+    const message = await Message.findById(messageId);
+    if (!message) {
+      return res.status(404).json({ message: "Message not found" });
     }
-        catch(error){
-            console.log("Error in sending message controller:" , error.message);
-            res.status(500).json({ message: "Failed to send message" });
 
-        }
-    }
-                    
+    const { text } = req.body;
+    const updatedMessage = await Message.findByIdAndUpdate(
+      messageId,
+      { text: text },
+      { new: true }
+    );
+
+    res.status(201).json({
+      message: "Message edited successfully",
+      updatedMessage,
+    });
+  } catch (error) {
+    console.log("Error in editing message controller:", error.message);
+    res.status(500).json({ message: "Failed to edit message" });
+  }
+};
+
+/* 
+DELETE
+TO DELETE THE MESSAGE
+ */
+
+export const deleteMessage = async (req, res) => {
+    try {
+        const { _id: messageId } = req.params;
+        const message = await Message.findById(messageId);
+        if (!message) {
+            return res.status(404).json({ message: "Message not found" });
+            }
+            await Message.findByIdAndDelete(messageId);
+            res.status(200).json({ message: "Message deleted successfully" });
+            } catch (error) {
+                console.log("Error in deleting message controller:", error.message);
+                res.status(500).json({ message: "Internel Server Error" });
+                }
+            }
